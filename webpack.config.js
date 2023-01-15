1af3da18700f98c8b833417dd750dadf66ec5aa3
Alexandr <a.platcev@ya.ru>
2023-01-16 00:25:25 +0300

    add hw

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: './src/index.html',
     }),
   ],
